bb0225c23641894ec5272a655fd62f3d9e8b82db
Julien Mailleret <8582351+jmlrt@users.noreply.github.com>
2023-08-24 17:05:52 +0200

    new test

--- .buildkite/scripts/test.sh
+++ .buildkite/scripts/test.sh
@@ -5,12 +5,12 @@ printf "BUILDKITE_REDACTED_VARS: %s\n\n" "$BUILDKITE_REDACTED_VARS"
 MY_SECRET="topsecret"
 printf "MY_SECRET: %s\n\n" "$MY_SECRET"
 
-VAR_SECRET="hello, VAR"
-printf "VAR_SECRET: %s\n\n" "$VAR_SECRET"
+# VAR_SECRET="hello, VAR"
+# printf "VAR_SECRET: %s\n\n" "$VAR_SECRET"
 
-VAR_1_SECRET="hello, VAR_1"
-printf "VAR_1_SECRET: %s\n\n" "$VAR_1_SECRET"
+# VAR_1_SECRET="hello, VAR_1"
+# printf "VAR_1_SECRET: %s\n\n" "$VAR_1_SECRET"
 
-VAR_2_SECRET="hello,
-VAR_2"
-printf "VAR_2_SECRET: %s\n\n" "$VAR_2_SECRET"
+# VAR_2_SECRET="hello,
+# VAR_2"
+# printf "VAR_2_SECRET: %s\n\n" "$VAR_2_SECRET"
