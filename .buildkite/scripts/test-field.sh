051a19867e970c96ba34b119e20a91e443428ff2
Julien Mailleret <8582351+jmlrt@users.noreply.github.com>
2023-08-24 15:46:14 +0200

    generate var name based on secret path

--- .buildkite/scripts/test-field.sh
+++ .buildkite/scripts/test-field.sh
@@ -2,15 +2,15 @@
 
 EXPECTED_MESSAGE="Hello from Vault"
 
-if [ -z "$MESSAGE" ]; then
+if [ -z "$TEST_MESSAGE_SECRET" ]; then
   echo "[ERROR] MESSAGE variable is empty or not defined."
   exit 1
 fi
 
-if [ "$MESSAGE" == "$EXPECTED_MESSAGE" ]; then
-  echo "Message is correct: $MESSAGE"
+if [ "$TEST_MESSAGE_SECRET" == "$EXPECTED_MESSAGE" ]; then
+  echo "Message is correct: $TEST_MESSAGE_SECRET"
   exit 0
 else
-  echo "[ERROR] Expected: $EXPECTED_MESSAGE - Got: $MESSAGE"
+  echo "[ERROR] Expected: $EXPECTED_MESSAGE - Got: $TEST_MESSAGE_SECRET"
   exit 1
 fi
--- .buildkite/scripts/test-json.sh
+++ .buildkite/scripts/test-json.sh
@@ -2,17 +2,17 @@
 
 EXPECTED_MESSAGE='"message": "Hello from Vault"'
 
-if [ -z "$MESSAGE" ]; then
+if [ -z "$TEST_SECRET" ]; then
   echo "[ERROR] MESSAGE variable is empty or not defined."
   exit 1
 fi
 
-is_expected_message=$(echo "$MESSAGE" | grep -c "$EXPECTED_MESSAGE")
+is_expected_message=$(echo "$TEST_SECRET" | grep -c "$EXPECTED_MESSAGE")
 
 if [ "$is_expected_message" -eq 1 ]; then
-  echo "Message is correct: $MESSAGE"
+  echo "Message is correct: $TEST_SECRET"
   exit 0
 else
-  echo "[ERROR] Expected: $EXPECTED_MESSAGE - Got: $MESSAGE"
+  echo "[ERROR] Expected: $EXPECTED_MESSAGE - Got: $TEST_SECRET"
   exit 1
 fi
