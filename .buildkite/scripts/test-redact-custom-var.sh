14a6d22783ca3b65437ab101a77466a8269e145f
David Natachanny <David.Natachanny@elastic.co>
2023-11-15 14:42:02 +0200

    add redaction for custom vars

---
+++ .buildkite/scripts/test-redact-custom-var.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+EXPECTED_MESSAGE="[REDACTED]"
+
+if [ -z "$MESSAGE" ]; then
+  echo "[ERROR] MESSAGE variable is empty or not defined."
+  exit 1
+fi
+
+if [ "$MESSAGE" == "$EXPECTED_MESSAGE" ]; then
+  echo "MESSAGE is correct: $MESSAGE"
+  exit 0
+else
+  echo "[ERROR] Expected: $EXPECTED_MESSAGE - Got: $MESSAGE"
+  exit 1
+fi
