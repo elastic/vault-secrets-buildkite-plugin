d0216a64ffaa92fa341ed2245e285c74d38946c5
navyau09 <navya.uppalapati@elastic.co>
2024-01-09 10:47:12 +0100

    Add testcase

--- .buildkite/scripts/test-path.sh
+++ .buildkite/scripts/test-path.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-EXPECTED_ENV_PATH_NAME='TEST_ELASTIC_CO_MESSAGE_SECRET'
+EXPECTED_MESSAGE='{"message":"Hello from Vault"}'
 
-if [ -z "$TEST_ELASTIC_CO_MESSAGE_SECRET" ]; then
-  echo "[ERROR] TEST_ELASTIC_CO_MESSAGE_SECRET variable is empty or not defined."
+if [ -z "$TEST_ELASTIC_CO_SECRET" ]; then
+  echo "[ERROR] TEST_ELASTIC_CO_SECRET variable is empty or not defined."
   exit 1
 fi
 
-if [ "$TEST_ELASTIC_CO_MESSAGE_SECRET" == "$EXPECTED_ENV_PATH_NAME" ]; then
-  echo "TEST_ELASTIC_CO_MESSAGE_SECRET is correct: $TEST_ELASTIC_CO_MESSAGE_SECRET"
+if [ "$TEST_ELASTIC_CO_SECRET" == "$EXPECTED_MESSAGE" ]; then
+  echo "TEST_ELASTIC_CO_SECRET is correct: $TEST_ELASTIC_CO_SECRET"
   exit 0
 else
-  echo "[ERROR] Expected: $EXPECTED_ENV_PATH_NAME - Got: $TEST_ELASTIC_CO_MESSAGE_SECRET"
+  echo "[ERROR] Expected: $EXPECTED_MESSAGE - Got: $TEST_ELASTIC_CO_SECRET"
   exit 1
 fi
