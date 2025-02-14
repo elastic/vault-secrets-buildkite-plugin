bd262e4639c6f9e9a8419e949b21b3870ced9542
alina-bacalete <alina.bacalete@elastic.co>
2025-02-14 17:42:11 +0200

    Add env_var in pipeline for test

--- .buildkite/scripts/test-format.sh
+++ .buildkite/scripts/test-format.sh
@@ -2,15 +2,15 @@
 
 EXPECTED_MESSAGE='{"message":"Hello from Vault"}'
 
-if [ -z "$TEST_FORMAT_SECRET" ]; then
-  echo "[ERROR] TEST_FORMAT_SECRET variable is empty or not defined."
+if [ -z "$MESSAGE" ]; then
+  echo "[ERROR] MESSAGE variable is empty or not defined."
   exit 1
 fi
 
-if [ "$TEST_FORMAT_SECRET" == "$EXPECTED_MESSAGE" ]; then
-  echo "TEST_FORMAT_SECRET is correct: $TEST_FORMAT_SECRET"
+if [ "$MESSAGE" == "$EXPECTED_MESSAGE" ]; then
+  echo "MESSAGE is correct: $MESSAGE"
   exit 0
 else
-  echo "[ERROR] Expected: $EXPECTED_MESSAGE - Got: $TEST_FORMAT_SECRET"
+  echo "[ERROR] Expected: $EXPECTED_MESSAGE - Got: $MESSAGE"
   exit 1
 fi
